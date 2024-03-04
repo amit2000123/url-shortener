622323cb4059a64d759deadc6e3c789127708c06
amit.prajapati <amit.prajapati@PINENOIL171919>
2024-03-04 16:25:07 +0530

    Add project files.

---
+++ url-shortener/Global.asax.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Routing;
+using System.Web.Security;
+using System.Web.SessionState;
+
+namespace url_shortener
+{
+    public class Global : System.Web.HttpApplication
+    {
+        void RegisterRoutes(RouteCollection routes)
+        {
+            routes.MapPageRoute("preview", "{ShortLink}", "~/Redirect.aspx");
+        }
+        protected void Application_Start(object sender, EventArgs e)
+        {
+            RegisterRoutes(RouteTable.Routes);
+        }
+    }
+}
---
+++ url-shortener/Redirect.aspx.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using url_shortener.Models;
+
+namespace url_shortener
+{
+    public partial class Redirect : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                try
+                {
+                    String ShortLink = "";
+                    ShortLink = Page.RouteData.Values["ShortLink"] as String;
+                    String originUrl = Functions.GetOriginUrl(ShortLink);
+                    if (originUrl != null)
+                    {
+                        result_area.InnerHtml = @"<h4>Your link is ready.</h4> 
+                                              <a class='btn btn-primary' href='" + originUrl + "'>Get Link</a>";
+                    }
+                    else
+                    {
+                        result_area.InnerHtml = @"<h4>Page Not Found</h4>";
+                    }
+                }
+                catch (Exception)
+                {
+
+                    Response.Redirect("~/");
+                }
+            }
+        }
+
+
+
+    }
+}
